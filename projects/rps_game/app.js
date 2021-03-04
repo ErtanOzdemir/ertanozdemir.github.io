721fc0d3befa4d7519cb916339a02d34b60fcc27
Ertan Özdemir <ertan_kubi@hotmail.com>
2021-03-04 22:03:44 +0300

    Rock Paper Scissors game added.

---
+++ projects/rps_game/app.js
@@ -0,0 +1,43 @@
+new Vue({
+    el: '#app',
+    data: {
+        scores: {
+            yourScore: 0,
+            computerScore: 0,
+            draw: 0
+        },
+        imageNumbers: {
+            userImage: "assets/0.png",
+            computerImage: "assets/0.png",
+            user: 0,
+            computer: 0,
+
+
+        }
+
+
+
+    },
+    methods: {
+        getRandom: function() {
+
+            this.imageNumbers.user = Math.floor(Math.random() * 3);
+            this.imageNumbers.computer = Math.floor(Math.random() * 3);
+            this.imageNumbers.userImage = "assets/" + this.imageNumbers.user + ".png";
+            this.imageNumbers.computerImage = "assets/" + this.imageNumbers.computer + ".png";
+
+            if ((this.imageNumbers.computer == 0 && this.imageNumbers.user == 0) || (this.imageNumbers.computer == 1 && this.imageNumbers.user == 1) || (this.imageNumbers.computer == 2 && this.imageNumbers.user == 2)) {
+                this.scores.draw++;
+            } else if (this.imageNumbers.computer == 0 && this.imageNumbers.user == 1) this.scores.yourScore++;
+            else if (this.imageNumbers.computer == 1 && this.imageNumbers.user == 0) this.scores.computerScore++;
+            else if (this.imageNumbers.computer == 1 && this.imageNumbers.user == 2) this.scores.yourScore++;
+            else if (this.imageNumbers.computer == 2 && this.imageNumbers.user == 1) this.scores.computerScore++;
+            else if (this.imageNumbers.computer == 2 && this.imageNumbers.user == 0) this.scores.yourScore++;
+            else if (this.imageNumbers.computer == 0 && this.imageNumbers.user == 2) this.scores.computerScore++;
+
+
+
+        }
+    },
+
+})
